6b6cc6ea5b4641b640613791ccd491eb226e99f9
netoADS <124839031+netoADS@users.noreply.github.com>
2023-02-08 23:47:29 +0000

    080220232047

---
+++ exe02.py
@@ -0,0 +1,7 @@
+nome = input("coloque o seu nome: ")
+rua = input("coloque o nome da sua Rua: ")
+cep = input("coloque o seu CEP: ")
+telefone = input("coloque o seu telefone: ")
+
+
+print("Você é: ", nome , "\n Rua: " , rua , "\n Cep: " , cep , "\nn Telefone: " , telefone)
